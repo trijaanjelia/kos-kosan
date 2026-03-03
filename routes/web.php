0f1c36e8e056910edbdff64594c19efa8a2243e9
Trija <trijaanjelia03@gmail.com>
2026-03-03 13:24:24 +0800

    Update authentication dan tambahkan file breeze

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KosController;
-use App\Models\Kos; // Tambahkan ini
 
 Route::get('/', function () {
-
-    $dataKos = Kos::all();
-
-    return view('welcome', compact('dataKos'));
+    return view('welcome');
 });
 
-Route::get('/kos',[KosController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/kos/create', [KosController::class, 'create']);
-Route::post('/kos', [KosController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
